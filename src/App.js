bf12123ca89713646edf09ca70184c904433c82e
Jinseok Lee <paskal3314@gmail.com>
2021-09-11 18:09:32 -0600

    Desktop mode implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {Button, Container} from 'react-bootstrap';
+
+import HeroImage from './images/illustration-hero.svg';
+import IconMusic from './images/icon-music.svg';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="wrapper">
+      <Container className="modal-container">
+        <Container fluid className="modal-hero">
+          <img src={HeroImage}/>
+        </Container>
+        <Container className="modal-body">
+          <h3>Order Summary</h3>
+          <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+
+          <Container className="modal-body__plan-container">
+            <img src={IconMusic}/>
+            <Container className="plan-detail-container">
+              <p><b>Annual Plan</b></p>
+              <p>$59.99/year</p>
+            </Container>
+            <a href="">Change</a>
+          </Container>
+          
+          <Container className="modal-body__proceed-container">
+            <Button className="btn_proceed">Proceed to Payment</Button>
+          </Container>
+          
+          <Container className="modal-body__cancel-container">
+            <a href="">Cancel Order</a>
+          </Container>
+        </Container>
+      </Container>
+    </Container>
   );
 }
 
